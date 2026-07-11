5e5a397b7e418bff1ca48f1aff7d7680996a090f
Naeem <naeem0n@github.com>
2026-07-11 14:59:26 +0500

    Fix React Error 418: suppress hydration warnings

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -70,11 +70,11 @@ export const Route = createRootRoute({
 
 function RootShell({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <head>
         <HeadContent />
       </head>
-      <body>
+      <body suppressHydrationWarning>
         {children}
         <Scripts />
       </body>
